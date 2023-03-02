337a77bf28c84310769d8d32bea9696ecb30eba9
Nicolasmgc <80405053+Nicolasmgc@users.noreply.github.com>
2023-03-02 23:30:24 +0100

    Interaction avec l'utilisateur

--- TPISEP/src/isep/app/TP1/Main.java
+++ TPISEP/src/isep/app/TP1/Main.java
@@ -1,4 +1,20 @@
 package isep.app.TP1;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int unEntier = scanner.nextInt();
+        float unReel = scanner.nextFloat();
+        System.out.println("J’ai recupere un entier: " + unEntier);
+        System.out.println("J’ai aussi recupere un reel: " + unReel);
+    }
+    // Le résultat devrait être "J’ai recupere un entier: $ " "J’ai aussi recupere un reel: &" avec $ l'entier qu'aura saisie l'utilisateur et & le réel qu'aura saisie l'utilisateur
+    // Il réceptionne les valeurs saisie par l'utilisateur, tout en vérifiant qu'il s'agit bien soit d'un réel ou un entier
+    // Nous obtenons le résultat attendu
+
+
 }
+
