41408dea035ea3ee1c37ab69e28e4d13c33bb7f1
Nicolasmgc <80405053+Nicolasmgc@users.noreply.github.com>
2023-03-02 23:49:39 +0100

    Somme de deux entiers

--- TPISEP/src/isep/app/TP1/Main.java
+++ TPISEP/src/isep/app/TP1/Main.java
@@ -13,10 +13,25 @@ public class Main {
         System.out.println("Bonjour, " + prenom);
         System.out.println("J’ai recupere un entier: " + unEntier);
         System.out.println("J’ai aussi recupere un reel: " + unReel);
+        somme();
     }
     // Le résultat devrait être "J’ai recupere un entier: $ " "J’ai aussi recupere un reel: &" avec $ l'entier qu'aura saisie l'utilisateur et & le réel qu'aura saisie l'utilisateur
     // Il réceptionne les valeurs saisie par l'utilisateur, tout en vérifiant qu'il s'agit bien soit d'un réel ou un entier
     // Nous obtenons le résultat attendu
+
+    public static void somme(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir le premier entier");
+        int premierEntier = scanner.nextInt();
+        System.out.println("Veuillez saisir le deuxieme entier");
+        int deuxiemeEntier = scanner.nextInt();
+        int somme = premierEntier + deuxiemeEntier;
+        System.out.println("La somme de " + premierEntier + " avec " + deuxiemeEntier +
+                " est egale a " + somme);
+
+
+    }
+
 
 
 }
